c4d8e75c54c4361232d360c1489e8a7221ee3b5c
ohoud-habib <ohoudhabib@gmail.com>
2024-09-29 09:25:05 +0300

    Initial Commit

---
+++ day15/day15App.swift
@@ -0,0 +1,17 @@
+//
+//  day15App.swift
+//  day15
+//
+//  Created by ohoud on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct day15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
